b639362d8d7e15f806ce6f1e4c58a8c7244fc5ca
ronnyr11 <ronnyrangkuti13@gmail.com>
2022-06-23 09:28:31 +0700

    Memasukan Beberapa folder di controllers & Views

---
+++ application/views/depan/v_about.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About</title>
+    <link rel="shorcut icon" href="<?php echo base_url().'theme/images/Smk.jpg'?>">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.min.css'?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/font-awesome.min.css'?>">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/simple-line-icons.css'?>">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick-theme.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/owl.carousel.min.css'?>">
+    <!-- Main CSS -->
+    <link href="<?php echo base_url().'theme/css/style.css'?>" rel="stylesheet">
+</head>
+
+<body>
+    <!--============================= HEADER =============================-->
+    
+    <div data-toggle="affix" style="border-bottom:solid 1px #f2f2f2;">
+        <div class="container nav-menu2">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar2 navbar-toggleable-md navbar-light bg-faded">
+                        <button class="navbar-toggler navbar-toggler2 navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                            <span class="icon-menu"></span>
+                        </button>
+                        <a href="<?php echo site_url('');?>" class="navbar-brand nav-brand2"><img class="img img-responsive" width="200px;" src="<?php echo base_url().'theme/images/Smk.jpg'?>"></a>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('');?>">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('about');?>">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('guru');?>">Guru</a>
+                                </li>
+                               
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('pengumuman');?>">Pengumuman</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('agenda');?>">Agenda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('download');?>">Download</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('galeri');?>">Gallery</a>
+                                </li>
+                                <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('contact');?>">Contact</a>
+                                </li>
+                          </ul>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+      <section>
+</section>
+<!--//END HEADER -->
+<!--//END ABOUT IMAGE -->
+<!--============================= WELCOME TITLE =============================-->
+<section class="welcome_about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>Kata Sambutan</h2>
+                <p>Puji syukur kami panjatkan ke hadirat Tuhan Yang Maha Esa atas karunia dan hidayah-Nya,
+                  sehingga kita semua dapat membaktikan segala hal yang kita miliki untuk kemajuan dunia pendidikan.
+                  Apapun bentuk dan sumbangsih yang kita berikan, jika dilandasi niat yang tulus tanpa memandang imbalan
+                  apapun akan menghasilkan mahakarya yang agung untuk bekal kita dan generasi setelah kita.
+                  Pendidikan adalah harga mati untuk menjadi pondasi bangsa dan negara dalam menghadapi perkembangan zaman.
+                </p>
+                <p>Hal ini seiring dengan penguasaan teknologi untuk dimanfaatkan sebaik mungkin,
+                  sehingga menciptakan iklim kondusif dalam ranah keilmuan. Dengan konsep yang kontekstual dan efektif,
+                  kami mengejewantahkan nilai-nilai pendidikan yang tertuang dalam visi misi Smk Muhammadiyah,
+                  sebagai panduan hukum dalam menjabarkan tujuan hakiki pendidikan.
+                </p>
+                </div>
+                <div class="col-md-5">
+                    <img src="<?php echo base_url().'theme/images/Smk.jpg'?>" class="img-fluid" alt="#">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--//END TESTIMONIAL -->
+   
+                <!--//END FOOTER -->
+                <!-- jQuery, Bootstrap JS. -->
+                <script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/tether.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+                <!-- Plugins -->
+                <script src="<?php echo base_url().'theme/js/slick.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/waypoints.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/counterup.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/owl.carousel.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/validate.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/tweetie.min.js'?>"></script>
+                <!-- Subscribe -->
+                <script src="<?php echo base_url().'theme/js/subscribe.js'?>"></script>
+                <!-- Script JS -->
+                <script src="<?php echo base_url().'theme/js/script.js'?>"></script>
+            </body>
+
+            </html>
---
+++ application/views/depan/v_agenda.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Agenda</title>
+    <link rel="shorcut icon" href="<?php echo base_url().'theme/images/Smk.jpg'?>">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.min.css'?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/font-awesome.min.css'?>">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/simple-line-icons.css'?>">
+    <!-- Calendar Css -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/fullcalendar.min.css'?>" />
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/owl.carousel.min.css'?>">
+    <!-- Main CSS -->
+    <link href="<?php echo base_url().'theme/css/style.css'?>" rel="stylesheet">
+</head>
+
+<body>
+  
+  </div>
+  <div data-toggle="affix" style="border-bottom:solid 1px #f2f2f2;">
+      <div class="container nav-menu2">
+          <div class="row">
+              <div class="col-md-12">
+                  <nav class="navbar navbar2 navbar-toggleable-md navbar-light bg-faded">
+                      <button class="navbar-toggler navbar-toggler2 navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                          <span class="icon-menu"></span>
+                      </button>
+                      <a href="<?php echo site_url('');?>" class="navbar-brand nav-brand2"><img class="img img-responsive" width="200px;" src="<?php echo base_url().'theme/images/Smk.jpg'?>"></a>
+                      <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                          <ul class="navbar-nav">
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('');?>">Home</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('about');?>">About</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('guru');?>">Guru</a>
+                              </li>
+                              
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('pengumuman');?>">Pengumuman</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('agenda');?>">Agenda</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('download');?>">Download</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('galeri');?>">Gallery</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="<?php echo site_url('contact');?>">Contact</a>
+                              </li>
+                        </ul>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+    <section>
+</section>
+<!--//END HEADER -->
+<!--============================= EVENTS =============================-->
+<section class="events">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h2 class="event-title">Agenda</h2>
+            </div>
+            <div class="col-md-8">
+                <!-- Nav tabs -->
+                <ul class="nav nav-tabs" role="tablist">
+                    <li class="nav-item nav-tab1">
+                        <a class="nav-link tab-list active" data-toggle="tab" href="#upcoming-events" role="tab">Agenda Terbaru </a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <div class="tab-pane active" id="upcoming-events" role="tabpanel">
+                  <?php foreach($data->result() as $row):?>
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <div class="event-date">
+                                    <h4><?php echo date("d", strtotime($row->agenda_tanggal));?></h4> <span><?php echo date("M Y", strtotime($row->agenda_tanggal));?></span>
+                                </div>
+                                <span class="event-time"><?php echo $row->agenda_waktu;?></span>
+                            </div>
+                            <div class="col-md-10">
+                                <div class="event-heading">
+                                    <h3><?php echo $row->agenda_nama;?></h3>
+                                    <p><?php echo $row->agenda_deskripsi;?></p>
+                                </div>
+                          </div>
+                      </div>
+                      <hr class="event-underline">
+                  </div>
+                <?php endforeach;?>
+
+      <div class="col-md-12 text-center">
+        <?php echo $page;?>
+    </div>
+</div>
+
+</div>
+</div>
+</div>
+</section>
+<!--//END EVENTS -->
+
+        <!--//END FOOTER -->
+        <!-- jQuery, Bootstrap JS. -->
+        <script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+        <script src="<?php echo base_url().'theme/js/tether.min.js'?>"></script>
+        <script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+        <!-- Plugins -->
+        <script src="<?php echo base_url().'theme/js/moment.min.js'?>"></script>
+        <script src="<?php echo base_url().'theme/js/fullcalendar.min.js'?>"></script>
+        <script src="<?php echo base_url().'theme/js/owl.carousel.min.js'?>"></script>
+        <script src="<?php echo base_url().'theme/js/validate.js'?>"></script>
+        <script src="<?php echo base_url().'theme/js/tweetie.min.js'?>"></script>
+        <!-- Subscribe -->
+        <script src="<?php echo base_url().'theme/js/subscribe.js'?>"></script>
+        <!-- Script JS -->
+        <script src="<?php echo base_url().'theme/js/script.js'?>"></script>
+    </body>
+
+    </html>
